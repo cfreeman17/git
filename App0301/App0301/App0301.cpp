46169b4e37e552fc9ee1ff22ae52a5ea26aa9fc6
Chandler Freeman <cfreeman0518@vbtc.net>
2017-03-20 10:40:10 -0400

    inital work on main function

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -1,11 +1,24 @@
 // App0301.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+string randomWord();
+string mixUp(const string& aWord);
+void welcomePlayer(const string& aJumble);
+void play(const string& aWord);
 
 int main()
 {
-	cout << "Hello World" << endl;
+	srand(static_cast<unsigned int>(time(0)));
+
+	string  word = randomWord();
+	string jumble = mixUp(word);
+	welcomePlayer(jumble);
+	play(word);
+
 	system("pause");
 	return 0;
 }

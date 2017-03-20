1f3199555dd01d17a93175f1186c5c27533601ca
Chandler Freeman <cfreeman0518@vbtc.net>
2017-03-20 09:19:13 -0400

    today

---
+++ App0301/App0301/App0301.cpp
@@ -0,0 +1,12 @@
+// App0301.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
+

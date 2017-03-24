66173b2a02a566e16f792cda832c5e349d14e4ec
Beau <soulotaku8@gmail.com>
2017-03-24 10:27:17 -0400

    merged mixUp

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -14,14 +14,35 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));
 
-	string  word = randomWord();
-	string jumble = mixUp(word);
-	welcomePlayer(jumble);
-	play(word);
+	//string  word = randomWord();
+	//string jumble = mixUp(word);
+	//welcomePlayer(jumble);
+	//play(word);
 
 	system("pause");
 	return 0;
 }
+
+
+
+//return a jumbled version of aWord
+string mixUp(const string& aWord)
+{
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
+}
+
+
+
 
 void play(const string& aWord)
 {

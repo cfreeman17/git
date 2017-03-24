02ac0f48bc8e2761584abcca55e604b26da61ac9
Chandler Freeman <cfreeman0518@vbtc.net>
2017-03-24 09:24:21 -0400

    Finished play

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -1,6 +1,9 @@
 // App0301.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 
 int main()
@@ -8,5 +11,23 @@ int main()
 	cout << "Hello World" << endl;
 	system("pause");
 	return 0;
+}
+
+void play(const string& aWord)
+{
+	string guess;
+	do {
+		cout << endl << "Your guess: ";
+		cin >> guess;
+
+		if ((guess != aWord) && (guess != "quit"))
+		{
+			cout << "Sorry, that's not it.";
+		}
+	} while ((guess != aWord) && (guess != "quit"));
+
+	if (guess == aWord)
+		cout << endl << "That's it! You guessed it!" << endl;
+	cout << endl << "Thanks for playing.\n" << endl;
 }
 
